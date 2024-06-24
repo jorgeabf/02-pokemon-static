59ee4b758b3560f6767eb5cd9a8b7cab6e43f8e4
jorgeabf <aportuvas@outlook.es>
2024-06-24 20:39:14 +0200

    chore: Actualizar título y descripción del sitio y agregar imagen predeterminada

--- src/consts/site-info.ts
+++ src/consts/site-info.ts
@@ -1,5 +1,7 @@
 export const SITE_INFO = {
-  SITE_TITLE: 'Pokemons Static App',
+  SITE_TITLE: 'Pokémons APP',
   SITE_DESCRIPTION:
-    'This is a static app that shows a list of pokemons and their details.'
+    'Esta es una aplicación estática que muestra una lista de pokemons con sus imágenes, sonidos y detalles.',
+  SITE_IMAGE:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
 }
